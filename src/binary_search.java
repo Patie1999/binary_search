40b3e87b2d3e80b204a27302870ec3eaf4a17875
PATIENCE OBVIOUS <patienceobvious@gmail.com>
2021-06-23 20:08:07 +0200

    First commit

--- src/binary_search.java
+++ src/binary_search.java
@@ -2,28 +2,38 @@ import java.util.Random;
 import java.util.Arrays;
 public class binary_search {
     public static void main(String[] args) {
+
+        //inializes an array of 30 integer numbers
         int[] array = new int[30];
 
-        generateRandomNumbersAndSort(array);
-        printArrayElements(array);
-        binarySearch(array, 6);
+        // adds randomly generated integer numbers to array
+        generateRandomNumbers(array);
+
+        // sorts array elements in ascending order
+        sortArrayElements(array);
+
+        // searches for 5 in array
+        binarySearch(array, 5);
 
     }
 
-    public static void generateRandomNumbersAndSort(int[] array) {
+    // adds randomly generated integer numbers to an array
+    public static void generateRandomNumbers(int[] array) {
         Random rand = new Random();
         for (int i = 0; i < 30; i++) {
             array[i] = rand.nextInt(100);
         }
-        Arrays.sort(array);
     }
 
-    public static void printArrayElements(int[] array) {
+    // sorts array elements in ascending order
+    public static void sortArrayElements(int[] array) {
+        Arrays.sort(array);
         for (int element : array) {
             System.out.println(element);
         }
     }
 
+    // searchs for integer n in the integer array
     public static void binarySearch(int[] arr, int n) {
         int last = arr.length - 1;
         int first = 0;
